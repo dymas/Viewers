b6d9d65d56e10068f7a7a4e6ed18a825a90c0cb2
Dymas <dymassousa@gmail.com>
2022-12-13 12:04:25 -0300

    Getting the resources folder

--- platform/viewer/public/preload.js
+++ platform/viewer/public/preload.js
@@ -1,11 +1,33 @@
 const fs = require('fs');
+const mimetypes = require('mime-types');
+const path = require('path');
 
 const { contextBridge } = require('electron');
 
+function LocalFileData(pathReceived) {
+  this.arrayBuffer = (() => {
+    var buffer = fs.readFileSync('./resources' + pathReceived);
+    var arrayBuffer = buffer.slice(
+      buffer.byteOffset,
+      buffer.byteOffset + buffer.byteLength
+    );
+    return [arrayBuffer];
+  })();
+
+  this.name = path.basename(pathReceived);
+
+  this.type = mimetypes.lookup(path.extname(pathReceived)) || undefined;
+}
+
 let files = [];
 
-fs.readdirSync('../../').forEach(file => {
-  files.push(file);
+fs.readdirSync('./resources').forEach(file => {
+  console.log(file);
+  console.log(path.extname(file));
+  if (path.extname(file) === '.dcm') {
+    const newfile = new LocalFileData(file);
+    files.push(newfile);
+  }
 });
 
 contextBridge.exposeInMainWorld('files', { ...files });
